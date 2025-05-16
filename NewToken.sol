1255dc65c8058e4adce44dd161364a10670c1d5c
tausif-cil <tausif@blocktechbrew.com>
2025-05-16 17:37:41 +0530

    Add Navada Token contract with ERC20, ERC20Burnable, ERC20Permit, and Ownable2Step functionality

--- NewToken.sol
+++ NewToken.sol
@@ -6,6 +6,7 @@ import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 import "@openzeppelin/contracts/access/Ownable2Step.sol";
  
+//Navada Token
 contract Navada is ERC20, ERC20Burnable, ERC20Permit, Ownable2Step {
     constructor(address _multiSigWallet)
         ERC20("Navada", "NAVADA")
